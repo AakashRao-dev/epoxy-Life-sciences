a841ceb3f5919fe0c25cb259d142e1f63af7bdc7
Aakash <aakash.codes02@gmail.com>
2022-12-24 13:32:03 +0530

    added extra images to productionSection2

--- src/components/Products/ProductsSection2/ProductsSection2.jsx
+++ src/components/Products/ProductsSection2/ProductsSection2.jsx
@@ -1,9 +1,20 @@
-import React from "react";
-import "./ProductSection2.css";
-import img1 from "../../../images/product1.jpeg";
-import img2 from "../../../images/product-2.jpeg";
-import img3 from "../../../images/product-3.jpeg";
-import img4 from "../../../images/product-4.jpeg";
+import React from 'react';
+import './ProductSection2.css';
+import img1 from '../../../images/product-1.jpeg';
+import img2 from '../../../images/product-2.jpeg';
+import img3 from '../../../images/product-3.jpeg';
+import img4 from '../../../images/product-4.jpeg';
+import img5 from '../../../images/product-5.jpeg';
+import img6 from '../../../images/product-6.jpeg';
+import img7 from '../../../images/product-7.jpeg';
+import img8 from '../../../images/product-8.jpeg';
+import img9 from '../../../images/product-9.jpeg';
+import img10 from '../../../images/product-10.jpeg';
+import img11 from '../../../images/product-11.jpeg';
+import img12 from '../../../images/product-12.jpeg';
+import img13 from '../../../images/product-13.jpeg';
+import img14 from '../../../images/product-14.jpeg';
+import img15 from '../../../images/product-15.jpeg';
 
 const ProductsSection2 = () => {
   return (
@@ -37,6 +48,105 @@ const ProductsSection2 = () => {
           <div className="box">
             <div className="imgbox">
               <img src={img4} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-5 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img5} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-6 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img6} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-7 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img7} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-8 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img8} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-9 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img9} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-10 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img10} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-11 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img11} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-12 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img12} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-13 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img13} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-14 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img14} alt="" />
+            </div>
+            <div className="price">
+              <h3>Price-149</h3>
+            </div>
+          </div>
+          {/* Image-15 */}
+          <div className="box">
+            <div className="imgbox">
+              <img src={img15} alt="" />
             </div>
             <div className="price">
               <h3>Price-149</h3>
